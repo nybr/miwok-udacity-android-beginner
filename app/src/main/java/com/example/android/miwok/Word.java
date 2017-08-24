ba555d2ec67df40a8b2c600b0ed397a52fcedfde
Nadja Yanna Braga <nadjayannabr@gmail.com>
2017-08-24 16:42:34 +0200

    primeira etapa finalizada

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,76 @@
+package com.example.android.miwok;
+
+/**
+ * Created by nadja on 23/08/2017.
+ */
+
+public class Word {
+
+    /**
+     * Default translation for the word
+     */
+    private String mDefaultTranslation;
+
+    /**
+     * Miwok translation for the word
+     */
+    private String mMiwokTranslation;
+
+    /**
+     * Drawable resource ID
+     */
+    private int mImageResourceId = NO_RESOURCE_PROVIDED_;
+
+    /**
+     * Audio resource ID
+     */
+    private int mAudioResourceId = NO_RESOURCE_PROVIDED_;
+
+    /** */
+    private static final int NO_RESOURCE_PROVIDED_ = -1;
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation   is the word in the Miwok language
+     */
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_RESOURCE_PROVIDED_;
+    }
+
+    public boolean hasAudio() {
+        return mAudioResourceId != NO_RESOURCE_PROVIDED_;
+    }
+}
---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,86 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nadja on 23/08/2017.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    private int mColorResourceId;
+
+    /**
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context The current context. Used to inflate the layout file.
+     * @param words   A List of Word objects to display in a list
+     */
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
+        super(context, 0, words);
+        mColorResourceId = colorResourceId;
+    }
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position    The position in the list of data that should be displayed in the
+     *                    list item view.
+     * @param convertView The recycled view to populate.
+     * @param parent      The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
+        ImageView iconView = (ImageView) listItemView.findViewById(R.id.word_image);
+        // Get the image resource ID from the current AndroidFlavor object and
+        // set the image to iconView
+        if (currentWord.hasImage()) {
+            iconView.setImageResource(currentWord.getImageResourceId());
+            iconView.setVisibility(View.VISIBLE);
+        } else {
+            iconView.setVisibility(View.GONE);
+        }
+
+        LinearLayout linearLayout = (LinearLayout) listItemView.findViewById(R.id.text_container);
+        linearLayout.setBackgroundResource(mColorResourceId);
+
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+}
